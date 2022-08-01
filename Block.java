979e35eca38c5f69a0f5b62c94eb044d224eeebf
Yinnfeng <71201322+Yinnfeng@users.noreply.github.com>
2022-08-01 13:28:33 +0100

    Add files via upload
    A simple Blockchain without the transaction and wallet components. The data here are simple texts. 
    Dependencies include Gson.

--- Block.java
+++ Block.java
@@ -1,22 +1,19 @@
-
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    public String merkleRoot;
-    public ArrayList<Transaction> transactions = new ArrayList<>(); //our data will be a simple message.
-    public long timeStamp; //as number of milliseconds since 1/1/1970.
+    private final String data;
+    private final long timeStamp; //as number of milliseconds since 1/1/1970.
     public int nonce;
 
     //Block Constructor.
-    public Block(String previousHash ) {
+    public Block(String data, String previousHash ) {
+        this.data =data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
-
-        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+        this.hash = calculateHash();
     }
 
     //Calculate new hash based on blocks contents
@@ -25,34 +22,16 @@ public class Block {
                 previousHash +
                         timeStamp +
                         nonce +
-                        merkleRoot
-        );
+                        data );
     }
-
     //Increases nonce value until hash target is reached.
     public void mineBlock(int difficulty) {
-        merkleRoot = StringUtil.getMerkleRoot(transactions);
-        String target = StringUtil.getDifficultyString(difficulty); //Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
     }
-
-    //Add transactions to this block
-    public void addTransaction(Transaction transaction) {
-        //process transaction and check if valid, unless block is genesis block then ignore.
-        if(transaction == null) return;
-        if((!"0".equals(previousHash))) {
-            if(( !transaction.processTransaction() )) {
-                System.out.println("Transaction failed to process. Discarded.");
-                return;
-            }
-        }
-
-        transactions.add(transaction);
-        System.out.println("Transaction Successfully added to Block");
-    }
-
 }
+
